1e391efcef0d089033f97d392db869f9fc9d1d54
samwray746 <S.Wray@bsms.ac.uk>
2024-09-02 15:03:28 +0100

    Update experimental_variables_function.py

--- gabor_task/experimental_variables_function.py
+++ gabor_task/experimental_variables_function.py
@@ -17,22 +17,9 @@ def experiment_variables(win):
     iti = [1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0] # inter-trial interval
     isi = [2, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0] # inter-stimulus interval
 
-    sequence_SD = ['S', 'D', 'D', 'S', 'D', 'S', 'S', 'D'] # basic systole-diastole sequence 
-    sequence_HF_LF = ['HF', 'LF', 'LF', 'HF', 'LF', 'HF', 'HF', 'LF'] # basic high-freq / low-freq sequence
+    diff_trial_conditions = ['s_hf', 's_lf', 'd_hf', 'd_lf']
 
-    # Sequence one, SD
-    sd_1 = (sequence_SD * (60 // len(sequence_SD))) + sequence_SD[:(60 % len(sequence_SD))]
-
-    # Sequence two, SD
-    sd_2 = ['D' if x == 'S' else 'S' for x in sd_1]
-
-    # Sequence three, freq
-    freq_1 = (sequence_HF_LF * (60 // len(sequence_HF_LF))) + sequence_HF_LF[:(60 % len(sequence_HF_LF))]
-
-    # Sequence four, freq
-    freq_2 = ['LF' if x == 'HF' else 'HF' for x in freq_1]
-
-    return pport_address_spike, trigger_code_spike, experiment_clock, countdown, fr, one_frame, trials_p_block, isi, iti, sd_1, sd_2, freq_1, freq_2
+    return pport_address_spike, trigger_code_spike, experiment_clock, countdown, fr, one_frame, trials_p_block, isi, iti, diff_trial_conditions
 
 
 
