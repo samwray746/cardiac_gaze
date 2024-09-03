7da628a2ca856d80ec896a8b00556f19779ece35
samwray746 <S.Wray@bsms.ac.uk>
2024-09-03 11:09:22 +0100

    Update experimental_variables_function.py

--- gabor_task/experimental_variables_function.py
+++ gabor_task/experimental_variables_function.py
@@ -14,12 +14,13 @@ def experiment_variables(win):
     one_frame = 1/fr
 
     trials_p_block = 60 # trials in a block 
+    training_trials = 12
     iti = [1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0] # inter-trial interval
     isi = [2, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0] # inter-stimulus interval
 
     diff_trial_conditions = ['s_hf', 's_lf', 'd_hf', 'd_lf']
 
-    return pport_address_spike, trigger_code_spike, experiment_clock, countdown, fr, one_frame, trials_p_block, isi, iti, diff_trial_conditions
+    return pport_address_spike, trigger_code_spike, experiment_clock, countdown, fr, one_frame, trials_p_block, training_trials, isi, iti, diff_trial_conditions
 
 
 
