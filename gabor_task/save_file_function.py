d86dfd0086c87452b212b6448ae6004b91935e1b
samwray746 <S.Wray@bsms.ac.uk>
2024-09-03 10:58:12 +0100

    Update save_file_function.py

--- gabor_task/save_file_function.py
+++ gabor_task/save_file_function.py
@@ -10,7 +10,7 @@ def save_file_modification(subject_id):
 
     sheet_names = ['training', 'block_1', 'block_2', 'block_3', 'summary_data']
 
-    heading_strings = ['trial_number', 'trial_type', 'angle_offset', 'left_right', 'clockwise_anticlockwise', 'correct_incorrect']
+    heading_strings = ['trial_number', 'trial_type', 'angle_offset', 'left_right', 'clockwise_anticlockwise', 'correct_incorrect', 'r_peak_time_1', 'r_peak_time_2', 'r_peak_time_3', 'r_peak_time_4', 'r_peak_time_5', 'mean_rr_int', 'predicted_rr_int']
     
     for ws_name in range(len(sheet_names)):
         data_wb.create_sheet(sheet_names[ws_name]) # creating a sheet to save each block 
