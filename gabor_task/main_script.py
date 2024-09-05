309b2628b7ee4944ab096268a1af99e736ffa87e
samwray746 <S.Wray@bsms.ac.uk>
2024-09-05 18:17:44 +0100

    Update main_script.py

--- gabor_task/main_script.py
+++ gabor_task/main_script.py
@@ -70,7 +70,7 @@ instructions(training_instructions_one, training_instructions_two, training_inst
 
 ### TRAINING BLOCKS ### 
 
-training_block(win, countdown, fixation_dot_grey, fixation_dot_yellow, fixation_dot_green, one_frame, training_trials, isi, iti, diff_trial_conditions)
+training_block(win, countdown, fixation_dot_grey, fixation_dot_yellow, fixation_dot_green, one_frame, training_trials, isi, iti, diff_trial_conditions, left_right, clockwise_anticlockwise, training_begins)
 
 
 
